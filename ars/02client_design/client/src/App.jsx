c7bc6198758080492ab768ad5a5b1338ad06e14e
Priyanka Daivagna <you@example.com>
2025-02-11 11:03:50 +0530

    changes

---
+++ ars/02client_design/client/src/App.jsx
@@ -0,0 +1,17 @@
+import Pageheader from "./header/Pageheader"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Pageheader/>
+     <h1>Hello world</h1>
+     <h2>hello</h2>
+  
+    </>
+  )
+}
+
+export default App
